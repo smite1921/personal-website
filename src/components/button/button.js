4b1e5eb709069a3a3e823007509b943363b2ce1e
Smit Patel <smitp505@gmail.com>
2021-05-06 09:14:03 -0400

    Fixed button hover bug for safari

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,11 +1,10 @@
 import React from "react";
 import styles from "./button.module.css";
-import {COLOR,SIZE,FONT} from "../../styles/constants";
 
 export default function Button({ text, link }) {
     return(
         <a href={link} className={styles.button}>
-            <span className={styles.block}/>{text}
+            <span data-text={text} className={styles.block}/>{text}
         </a>
     );
 }
