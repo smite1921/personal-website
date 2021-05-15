9547e6c04832f08a4dfc3130cefec210f90b7fd0
Smit Patel <smitp505@gmail.com>
2021-05-14 22:23:57 -0400

    Changes to improve SEO (meta tags). Added favicon images for different devices.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import Home from "./home/home";
 import Experience from "./experience/experience";
 import Project from "./projects/projects";
 import Contact from "./contact/contact";
+import Head from "../components/head/head";
 import styles from "./index.module.css";
 
 export default function Index() {
   return (
     <div>
+      <Head/>
       <div className={styles.nav}>
         <Nav/>
       </div>
